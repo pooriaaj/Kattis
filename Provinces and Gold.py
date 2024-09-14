d1f428ad05b8d74061432cccbc6706c33dda69d4
pooriaaj <83242622+pooriaaj@users.noreply.github.com>
2024-09-13 22:49:19 -0400

    Provinces and Gold.py

--- Provinces and Gold.py
+++ Provinces and Gold.py
@@ -1,7 +1,6 @@
 gold, silver, copper = map(int, input().split())
 power = 3 * gold + 2 * silver + copper
 
-# Determine the status
 if power >= 8:
     status = 'Province'
 elif power >= 5:
@@ -10,16 +9,12 @@ elif power >= 2:
     status = 'Estate'
 else:
     status = ''
-
-# Determine the card
 if power >= 6:
     card = 'Gold'
 elif power >= 3:
     card = 'Silver'
 else:
     card = 'Copper'
-
-# Print the result
 if status:
     print(f"{status} or {card}")
 else:
