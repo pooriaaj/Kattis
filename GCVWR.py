7c73085a5c7403d8089de96ef8d3c27cdf0755c8
pooriaaj <83242622+pooriaaj@users.noreply.github.com>
2024-09-13 23:15:54 -0400

    GCVWR.py

--- GCVWR.py
+++ GCVWR.py
@@ -1,1 +1,4 @@
-print(int(0.9 * (g - t) - sum(map(int, input().split())))) if (g, t, n := map(int, input().split())) else None
+g, t, n = map(int, input().split())
+items = list(map(int, input().split()))
+result = int(0.9 * (g - t) - sum(items))
+print(result)
